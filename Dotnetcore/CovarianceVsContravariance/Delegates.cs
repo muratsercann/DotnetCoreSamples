1162ceec96198eaf0e737bf1d2502c37534c1e12
Murat Sercan <muratsercann@gmail.com>
2024-01-15 19:30:33 +0300

    Covariance vs Contravariance Sample

---
+++ Dotnetcore/CovarianceVsContravariance/Delegates.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CovarianceVsContravariance
+{
+
+    public static class Delegates
+    {
+        public static void Run()
+        {
+            //Consume(new Base());
+            //Consume(new Derived());
+
+            //Action<Base> actBase = Consume;
+            //actBase(new Base());
+            //actBase(new Derived());
+
+            //Action<Derived> actDerived = Consume;
+            //actDerived(new Derived());
+
+            //actDerived = actBase;// contravariance olduğu için Base -> Derived atanabildi.
+
+            /*Console.WriteLine("\n\nMyAction d = new MyAction(Consume);");
+            MyAction d = Consume<Base>;
+            d(new Base());
+            d(new Derived());
+
+            Console.WriteLine("\n\nMyGenericAction<Base> e = new MyGenericAction<Base>(Consume);");
+            MyGenericAction<Base> e = new MyGenericAction<Base>(Consume<Base>);
+            e(new Base());
+            e(new Derived());*/
+
+            // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+            // |||||||||||||||||||||||||||||||||||||||||
+            // vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv
+
+            MyGenericAction<Derived> consOfDerived = null;
+            MyGenericAction<Base> consOfBase = Consume<Base>;
+            consOfDerived = consOfBase;
+            //Bu delegeyi "in" ile contravariance yapmamız
+            //delegate void MyGenericAction<in T>(T b);
+            //Base tipindeki delegeyi derived tipindekine atama yaparken işimize yarıyor :
+        }
+
+        delegate void MyAction(Base b);
+        delegate void MyGenericAction<in T>(T b);
+
+        public static void Consume<T>(T input)
+        {
+            Console.WriteLine($"{input.GetType()} is being consumed !");
+        }
+
+        public static Base Produce()
+        {
+            return new Base();
+        }
+    }
+
+    public class Base { }
+
+    public class Derived : Base { }
+}
---
+++ Dotnetcore/CovarianceVsContravariance/Program.cs
@@ -0,0 +1,97 @@
+﻿
+internal class Program
+{
+    delegate void MyAction(Base b);
+    delegate void MyGenericAction<in T>(T b);
+
+    private static void Main(string[] args)
+    {
+        #region !! Important
+
+        CovarianceVsContravariance.Delegates.Run();
+
+        IEnumerable<Derived> aaa = new List<Derived>();
+        IEnumerable<Base> bbb = aaa;
+
+        Action<Base> ccc = (target) => { Console.WriteLine(target.GetType().Name); };
+        Action<Derived> ddd = ccc;
+        ddd(new Derived());
+
+        #endregion
+
+        #region Basics
+        Base x = new Base();
+        Base y = new Derived();
+        Derived z = new Derived();
+        //z = new Base();//compile error
+
+        IProducer<Base> prodOfBase = new Producer<Base>();
+        Base a = prodOfBase.Create();
+        //Derived b = prodOfBase.Create();// compile error
+
+        IProducer<Derived> prodOfDerived = new Producer<Derived>();
+        Derived b = prodOfDerived.Create();
+        Base c = prodOfDerived.Create();
+
+        IConsumer<Base> consOfBase = new Consumer<Base>();
+        consOfBase.Consume(new Base());
+        consOfBase.Consume(new Derived());
+
+        IConsumer<Derived> consOfDerived = new Consumer<Derived>();
+        consOfDerived.Consume(new Derived());
+        //consOfDerived.Consume(new Base()); //Compile Error
+
+        ///////////////////////////////////////////////////////////////////
+
+        IProducer<Base> p = prodOfBase;         // IProducer<Base>
+        IProducer<Base> q = prodOfDerived;      // IProducer<Derived>
+        IProducer<Derived> r = prodOfDerived;   // IProducer<Derived>
+        //IProducer<Derived> r = prodOfBase;    // IProducer<Base> Error!!!!
+
+        IConsumer<Derived> t = consOfDerived;   //IConsumer<Derived>
+        //beacuse we write IConsumer<in target> , - IConsumer<Derived> u = consOfBase; - works
+
+        IConsumer<Derived> u = consOfBase;  // ÖNEMLİ !!!
+
+        Console.WriteLine("\n\nIConsumer<Derived> u = consOfBase;");
+        Console.Write($"u.Consume(new Base()); : Error: ");
+        //u.Consume(new Base()); // ! Error !
+        Console.Write($"consOfBase.Consume(new Derived()); : ");
+        u.Consume(new Derived());
+
+        IConsumer<Base> v = consOfBase;         //IConsumer<Base>
+                                                //IConsumer<Base> w = consOfDerived;    //IConsumer<Derived>   
+
+        #endregion
+    }
+}
+
+class Producer<T> : IProducer<T>
+{
+    public T Create()
+    {
+        return default(T);
+    }
+}
+
+class Consumer<T> : IConsumer<T>
+{
+    public void Consume(T input)
+    {
+        Console.WriteLine(typeof(T));
+    }
+}
+
+public interface IProducer<out T>
+{
+    T Create();
+}
+
+public interface IConsumer<in T>
+{
+    void Consume(T input);
+}
+
+public class Base { }
+
+public class Derived : Base { }
