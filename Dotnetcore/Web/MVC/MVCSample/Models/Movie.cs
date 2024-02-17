ae3d847b3356cc06f8485d3b1cce6d78a7207742
Murat Sercan <muratsercann@outlook.com>
2024-02-18 00:42:25 +0300

    Seeding data with SeedData.cs. Adding New Field to Model(CodeFirst), Adding Movie Search. DataAnnotations, [ValidateAntiForgeryToken] attribute, Input Tag Helper, Validation Tag Helper, Form Tag Helper, Label Tag Helper, Model Binding, Model Validation, Overloading Delete, Create actions with the same ActionName with attribute etc.

--- Dotnetcore/Web/MVC/MVCSample/Models/Movie.cs
+++ Dotnetcore/Web/MVC/MVCSample/Models/Movie.cs
@@ -1,14 +1,31 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MVCSample.Models
 {
     public class Movie
     {
         public int Id { get; set; }//is required by the database for the primary key.
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
         public string? Title { get; set; }
+        [Display(Name = "Release Date")]
+        //[DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         [DataType(DataType.Date)]//specifies the type of the data only as the Date.
         public DateTime ReleaseDate { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        [Required]
+        [StringLength(30)]
         public string? Genre { get; set; }
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18, 2)")]
         public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+        [StringLength(5)]
+        [Required]
+        public string? Rating { get; set; }
+
     }
 }
--- Dotnetcore/Web/MVC/MVCSample/Program.cs
+++ Dotnetcore/Web/MVC/MVCSample/Program.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Localization;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using MVCSample.Data;
+using MVCSample.Models;
+using System.Globalization;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<MVCSampleContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("MVCSampleContext") ?? throw new InvalidOperationException("Connection string 'MVCSampleContext' not found.")));
@@ -9,6 +12,26 @@ builder.Services.AddDbContext<MVCSampleContext>(options =>
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
+
+
+////Globalization
+//var defaultCulture = new CultureInfo("tr-TR");
+//var localizationOptions = new RequestLocalizationOptions
+//{
+//    DefaultRequestCulture = new RequestCulture(defaultCulture),
+//    SupportedCultures = new List<CultureInfo> { defaultCulture },
+//    SupportedUICultures = new List<CultureInfo> { defaultCulture }
+//};
+//app.UseRequestLocalization(localizationOptions);
+
+
+//Seed Data
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);    SeedData.Initialize(services);
+}
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -28,5 +51,4 @@ app.UseAuthorization();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
 app.Run();
